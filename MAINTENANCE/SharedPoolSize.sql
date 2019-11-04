728c6312cca75c579b21f784d499f508b2fd4528
Martin Georgiev <mggeorgiev@gmail.com>
2019-11-04 23:37:09 +0200

    Update SharedPoolSize.sql

--- MAINTENANCE/SharedPoolSize.sql
+++ MAINTENANCE/SharedPoolSize.sql
@@ -2,6 +2,7 @@ show parameter shared_pool;
 
 select * from v$sga;
 
+--Large Pool
 select 
    name, 
    sum(bytes) 
@@ -16,20 +17,27 @@ select * from
    (select 
       pool, 
       name, 
-      bytes/1024/1024/1024 GB 
+      round(bytes/1024/1024,0) MB 
    from 
       v$sgastat 
    order by 
-      GB  desc); 
+      MB  desc); 
+
+--OR
+Select 
+	POOL, Name, Round(bytes/1024/1024,0) Memory_In_MB
+From V$sgastat
+ORDER BY Memory_In_MB DESC;
+
 
  SELECT * FROM v$SGASTAT;
  
-SELECT SUM(BYTES)/(1024*1024*1024) FROM V$DATAFILE;
+SELECT SUM(BYTES)/(1024*1024*1024) GB FROM V$DATAFILE;
 
-SELECT SUM(BYTES)/(1024*1024*1024) FROM V$LOGFILE a, V$LOG b
+SELECT SUM(BYTES)/(1024*1024*1024) GB FROM V$LOGFILE a, V$LOG b
 WHERE a.group#=b.group#;
 
-SELECT SUM(BYTES)/(1024*1024*1024) FROM V$TEMPFILE WHERE
+SELECT SUM(BYTES)/(1024*1024*1024) GB FROM V$TEMPFILE WHERE
 status='ONLINE';
 
 --ALTER SYSTEM SET SHARED_POOL_SIZE=400M SCOPE=BOTH;
@@ -43,3 +51,25 @@ SELECT *
   FROM V$SGASTAT
  WHERE name = 'free memory'
    AND pool = 'shared pool';
+
+select sum(value)/1024/1024/1024 Total_size_In_GB from V$sga;
+
+select round(sum(value)/1024/1024,0) Total_size_In_MB from V$sga;
+
+Select 
+	POOL, Round(bytes/1024/1024,0) Free_Memory_In_MB
+From V$sgastat
+Where Name Like '%free memory%';
+
+
+ Select sum(bytes/1024/1024) Free_Memory_In_MB
+ From V$sgastat
+ Where Name Like '%free memory%';
+
+--Advisor
+select * from v$sga_target_advice order by sga_size;
+
+--The amount of memory allocated to each dynamic component is displayed using the V$MEMORY_DYNAMIC_COMPONENTS view.
+SELECT  component, current_size, min_size, max_size
+FROM    v$memory_dynamic_components
+WHERE   current_size != 0;
