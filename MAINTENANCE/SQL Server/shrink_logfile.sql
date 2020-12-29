9ef5866e6a58f5d88b2679802015769a3d714f88
Martin Georgiev <mggeorgiev@gmail.com>
2020-12-29 12:22:36 +0100

    Create shrink_logfile.sql

---
+++ MAINTENANCE/SQL Server/shrink_logfile.sql
@@ -0,0 +1,22 @@
+USE [lego] ;   
+ALTER DATABASE [lego] SET RECOVERY simple;
+
+SELECT
+     LogicalName = dbf.name
+    ,FileType = dbf.type_desc
+    ,FilegroupName = fg.name
+    ,PhysicalFileLocation = dbf.physical_name
+    ,FileSizeMB = CONVERT(DECIMAL(10,2),dbf.size/128.0)
+    ,UsedSpaceMB = CONVERT(DECIMAL(10,2),dbf.size/128.0 - ((dbf.size/128.0)
+               - CAST(FILEPROPERTY(dbf.name, 'SPACEUSED') AS INT) /128.0))
+    ,FreeSpaceMB = CONVERT(DECIMAL(10,2),dbf.size/128.0
+           - CAST(FILEPROPERTY(dbf.name, 'SPACEUSED') AS INT)/128.0)
+FROM sys.database_files dbf
+LEFT JOIN sys.filegroups fg ON dbf.data_space_id = fg.data_space_id
+ORDER BY dbf.type DESC, dbf.name;
+
+USE [lego];
+DBCC SHRINKFILE(lego_log, 1);
+
+USE [lego] ;  
+ALTER DATABASE [lego] SET RECOVERY FULL ; 
