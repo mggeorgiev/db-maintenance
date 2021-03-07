0b5ea1a3c0f4c52488e2794f944e82261b8220b1
Martin Georgiev <mggeorgiev@gmail.com>
2021-03-07 23:04:54 +0100

    Update set_statistics.sql

--- MAINTENANCE/SQL Server/optimize/set_statistics.sql
+++ MAINTENANCE/SQL Server/optimize/set_statistics.sql
@@ -1,5 +1,21 @@
 SET STATISTICS IO, TIME ON;
 
+USE AdventureWorks2017
+GO
+
 SELECT [SalesOrderID] ,[CarrierTrackingNumber] ,[OrderQty] ,[ProductID], [UnitPrice] ,[ModifiedDate]
 FROM [AdventureWorks2017].[Sales].[SalesOrderDetail]
 WHERE [ModifiedDate] > '2012/01/01' AND [ProductID] = 772;
+
+CREATE NONCLUSTERED INDEX [IX_SalesOrderDetail_ProductID] ON [Sales].[SalesOrderDetail]
+(
+    [ProductID] ASC
+)
+WITH (DROP_EXISTING = on);
+go
+
+CREATE NONCLUSTERED INDEX [IX_SalesOrderDetail_ProductID]
+ON [Sales].[SalesOrderDetail] ([ProductID],[ModifiedDate])
+INCLUDE ([CarrierTrackingNumber],[OrderQty],[UnitPrice])
+WITH (DROP_EXISTING = on);
+GO
